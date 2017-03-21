fe0ffcc46db5ea92d08b34019a2077ffe67cf6a8
heokjin <heokjin@gmail.com>
2017-03-21 15:23:43 +0900

    c

--- public/js/contact_me.js
+++ public/js/contact_me.js
@@ -20,14 +20,15 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "././mail/contact_me.php",
-                type: "POST",
-                data: {
-                    name: name,
-                    phone: phone,
-                    email: email,
-                    message: message
-                },
+                url: "././public/mail/contact_me.php",
+                type: "GET",
+				data: "name="+name+"&phone="+phone+"&email="+email+"&message="+message,
+//                data: {
+//                    name: name,
+//                    phone: phone,
+//                    email: email,
+//                    message: message
+//                },
                 cache: false,
                 success: function() {
                     // Success message
--- public/mail/contact_me.php
+++ public/mail/contact_me.php
@@ -1,22 +1,25 @@
 <?php
 // Check for empty fields
-if(empty($_POST['name'])      ||
-   empty($_POST['email'])     ||
-   empty($_POST['phone'])     ||
-   empty($_POST['message'])   ||
-   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+if(empty($_GET['name'])      ||
+   empty($_GET['email'])     ||
+   empty($_GET['phone'])     ||
+   empty($_GET['message'])   ||
+   !filter_var($_GET['email'],FILTER_VALIDATE_EMAIL))
    {
    echo "No arguments Provided!";
    return false;
    }
    
-$name = strip_tags(htmlspecialchars($_POST['name']));
-$email_address = strip_tags(htmlspecialchars($_POST['email']));
-$phone = strip_tags(htmlspecialchars($_POST['phone']));
-$message = strip_tags(htmlspecialchars($_POST['message']));
+
+$name = strip_tags(htmlspecialchars($_GET['name']));
+echo "<script>console.log( 'Debug Objects: " . $name . "' );</script>";
+
+$email_address = strip_tags(htmlspecialchars($_GET['email']));
+$phone = strip_tags(htmlspecialchars($_GET['phone']));
+$message = strip_tags(htmlspecialchars($_GET['message']));
    
 // Create the email and send the message
-$to = 'yourname@yourdomain.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = 'heokjin@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
 $headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
